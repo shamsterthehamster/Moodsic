908b97e1f1816dd2cdd30b962a85491755810044
He Zhang <rbczhang90@gmail.com>
2018-09-29 17:42:32 -0700

    Add firebase but not working

--- moodsic_app/lib/main.dart
+++ moodsic_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'dart:async';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 void main() => runApp(MyApp());
 
@@ -37,24 +38,40 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   File _image;
+  List<Face> allFaces;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
 
     setState(() {
       _image = image;
+      scanImage(_image);
     });
+  }
+
+  Future scanImage(File _image) async {
+    final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(_image);
+    final FaceDetector faceDetector = FirebaseVision.instance.faceDetector(FaceDetectorOptions(enableClassification: true));
+    List<Face> allFaces = await faceDetector.detectInImage(visionImage);
+    for (Face face in allFaces) {
+      if (face.smilingProbability != null) {
+        final double smileProb = face.smilingProbability;
+        if(smileProb>=0.80){
+          print("Happy!");
+        }
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text('Image Picker Example'),
+          title: new Text('Welcome to Moodsic!'),
         ),
         body: new Center(
           child: _image == null
-              ? new Text('No image selected.')
+              ? new Text('Moodsic\nMusic to Fit Your Mood')
               : new Image.file(_image),
         ),
         floatingActionButton: new FloatingActionButton(
